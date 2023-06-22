f61076fece1936ad9c05ef2369945725838487e9
William John Austin <wiaustin@my365.bellevue.edu>
2023-06-22 13:11:29 -0400

    Update austin-projections.js

--- week-5/austin-projections.js
+++ week-5/austin-projections.js
@@ -15,13 +15,14 @@
         "email":"bdylan@gmail.com", 
         "dateCreated": new Date()}
         )
+        
  /**
  * Updates the email value for the Mozart Composer Document
  */
     db.users.updateOne({"employeeId":"1009"}, {"$set":{"email":"mozart@me.com"}})
 
+
  /**
  * Displays all the documents in our users collection, but only shows the First Name, Last Name, and email. 
  */
-
     db.users.aggregate([{$project : {firstName: 1 , lastName: 1, _id:0, email:1}}])
