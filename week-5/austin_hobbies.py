ad0ad843f8fe7a9cde0bd294c38ea633adf637f4
William John Austin <wiaustin@my365.bellevue.edu>
2023-06-23 18:28:56 -0400

    Create austin_hobbies.py

---
+++ week-5/austin_hobbies.py
@@ -0,0 +1,7 @@
+#################################################
+# Title: Exercise 5.3 - Python Conditionals, Lists and Loops
+# Author: William Austin
+# Date: June 22, 2023
+# Description: A Python exercise using new knowledge of conditionals, lists, and loops
+#################################################
+
