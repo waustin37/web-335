bdb661a686391964b6d4d8c3cb56a8b963fc625f
William John Austin <wiaustin@my365.bellevue.edu>
2023-06-25 13:36:50 -0400

    Update austin_hobbies.py

--- week-5/austin_hobbies.py
+++ week-5/austin_hobbies.py
@@ -5,3 +5,19 @@
 # Description: A Python exercise using new knowledge of conditionals, lists, and loops
 #################################################
 
+
+# Creating variables for array of Hobbies and Days of the Week
+hobbies = ["Video Games", "Cooking", "Baking", "Weight Lifting", "Acting"]
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+#Iterates over the hobbies and prints them to the console
+for x in hobbies:
+    print(x)
+
+
+#Iterates over the days and prints a message depending on whether or not it's the weekend
+for day in days:
+    if day == "Saturday" or day =="Sunday":
+        print("You're off today! Enjoy your hobbies!")
+    else:
+        print("GET TO WORK!!")
