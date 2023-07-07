14f868d04694ab3d662b5ed1b6429e486362a46d
William John Austin <wiaustin@my365.bellevue.edu>
2023-07-07 19:01:48 -0400

    Adjusting File Names to fit Python Guidelines and Adding Week 7

--- week_6/austin_usersp1.py
+++ week_6/austin_usersp1.py
@@ -2,9 +2,11 @@
 # Title: Exercise 6.3 - 
 # Author: William Austin
 # Date: July, 1 2023
-# Description: 
+# Description: Python and Mongo Exercise 
 #################################################
 
+
+'''import the Mongo Client'''
 from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://web335_user:s3cret@cluster0.hkbvlmn.mongodb.net/?retryWrites=true&w=majority')
@@ -17,7 +19,7 @@ for user in db.users.find():
     print(user)
 
 #Display the user with the following employee ID
-print(db.users.find_one({"employeeID":"1011"}))
+print(db.users.find_one({"employeeId":"1011"}))
 
 #Display the user with the following last name
 print(db.users.find_one({"lastName":"Mozart"}))
---
+++ week_7/austin_usersp2.py
@@ -0,0 +1,54 @@
+#################################################
+# Title: Exercise 7.3 - 
+# Author: William Austin
+# Date: July, 7 2023
+# Description: Exercise practicing usign Python with MongoDB
+# Work Cited : WEB 335 Python Guide
+#################################################
+
+
+#This will import the Mongo Client
+from pymongo import MongoClient
+import datetime
+
+#This will build a connection string to connect to
+client = MongoClient('mongodb+srv://web335_user:s3cret@cluster0.hkbvlmn.mongodb.net/?retryWrites=true&w=majority')
+
+db = client['web335DB']
+
+#First Create a New User Object to Insert
+gaga = {
+    "firstName":"Stefani",
+    "lastName":"Germanotta",
+    "employeeId":"1014",
+    "email":"ladygaga@joanna.com",
+    "dateCreated": datetime.datetime.utcnow()
+}
+
+#Inserts that object into our database as a new document
+gaga_user_id = db.users.insert_one(gaga).inserted_id
+print(gaga_user_id)
+
+#Displays the newly created document
+print(db.users.find_one({"employeeId":"1014"}))
+
+#Code to update the email of our new document 
+db.users.update_one(
+    {"employeeId":"1014"},
+    {
+        "$set": {
+            "email": "MotherMonster@gaga.com"
+        }
+    }
+)
+
+#Displays the newly updated document
+print(db.users.find_one({"employeeId":"1014"}))
+
+result = db.users.delete_one({
+    "employeeId":"1014"
+})
+print(result)
+
+#Search for the deleted document to prove it's gone
+print(db.users.find_one({"employeeId":"1014"}))
